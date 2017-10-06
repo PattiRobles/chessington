16f6ad00cf715ed84eb14329d1970c9a2b501883
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 15:37:16 +0100

    blocking logic for pawn - cherry pick Red 8

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,25 +11,35 @@ export default class Pawn extends Piece {
         let location = board.findPiece(this)
         const moves = []
 
-        let blocking1Square = board.getPiece(Square.at(location.row + 1, location.row))
-        let blocking2Squares = board.getPiece(Square.at(location.row + 2, location.col))
+
+
 
         if (this.player === Player.WHITE) {
-            //if W pawn in initial line, it can move 2 sq fwd
 
-            if (location.row === 1) {
-                moves.push(Square.at(location.row + 2, location.col))
+            let blocking1Square = board.getPiece(Square.at(location.row + 1, location.col))
+            let blocking2Squares = board.getPiece(Square.at(location.row + 2, location.col))
+
+            if (!blocking1Square) {
+                if (location.row === 1 && !blocking2Squares) {
+                    moves.push(Square.at(location.row + 2, location.col))
+                }
+                moves.push(Square.at(location.row + 1, location.col))
             }
 
-            //else, if already moved, and also for 1st move, it can ove only one square fwd
-            moves.push(Square.at(location.row + 1, location.col))
+
         } else {
-            //if B pawn in initial line, it can move 2 sq fwd
-            if (location.row === 6) {
-                moves.push(Square.at(location.row - 2, location.col))
+            let blocking1Square = board.getPiece(Square.at(location.row - 1, location.col))
+            let blocking2Squares = board.getPiece(Square.at(location.row - 2, location.col))
+
+            if (!blocking1Square) {
+                if (location.row === 6 && !blocking2Squares) {
+                    moves.push(Square.at(location.row - 2, location.col))
+                }
+                moves.push(Square.at(location.row - 1, location.col))
             }
-            moves.push(Square.at(location.row - 1, location.col))
+
         }
         return moves
     }
 }
+
