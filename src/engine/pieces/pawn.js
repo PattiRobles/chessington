5250839633d6be73aaa8b5e25d2c485fc10bac72
Patricia Robles <patriciarobles.tech@gmail.com>
2023-01-23 14:31:54 +0000

    rook logic -cherry pick red2

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -10,12 +10,21 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         const moves = []
+
+        let blocking1Square = board.getPiece(Square.at(location.row + 1, location.row))
+        let blocking2Squares = board.getPiece(Square.at(location.row + 2, location.col))
+
         if (this.player === Player.WHITE) {
+            //if W pawn in initial line, it can move 2 sq fwd
+
             if (location.row === 1) {
                 moves.push(Square.at(location.row + 2, location.col))
             }
+
+            //else, if already moved, and also for 1st move, it can ove only one square fwd
             moves.push(Square.at(location.row + 1, location.col))
         } else {
+            //if B pawn in initial line, it can move 2 sq fwd
             if (location.row === 6) {
                 moves.push(Square.at(location.row - 2, location.col))
             }
