b4b6ff6b00df5e3cf4cecaaa28d433a96cd63dd0
Patricia Robles <patriciarobles.tech@gmail.com>
2023-01-23 19:08:20 +0000

    pawn capture of opponent pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -12,8 +12,21 @@ export default class Pawn extends Piece {
         const moves = []
 
         if (this.player === Player.WHITE) {
+            //last row
             if (location.row === 7) return moves
 
+            //capture black piece
+            //HOW to rule out king from capture ( along the lines of "pieceToCapturRight != Piece.King" ???)
+            let pieceToCaptureRight = board.getPiece(Square.at(location.row + 1, location.col + 1))
+            let pieceToCaptureLeft = board.getPiece(Square.at(location.row + 1, location.col - 1))
+
+            if (pieceToCaptureLeft && pieceToCaptureLeft.player === Player.BLACK || pieceToCaptureRight && pieceToCaptureRight.player === Player.BLACK) {
+                if (location.col !== 7) moves.push(Square.at(location.row + 1, location.col + 1))
+                if (location.col !== 0) moves.push(Square.at(location.row + 1, location.col - 1))
+            }
+
+
+            //limit movement if blocked
             let blocking1Square = board.getPiece(Square.at(location.row + 1, location.col))
             let blocking2Squares = board.getPiece(Square.at(location.row + 2, location.col))
 
@@ -24,10 +37,20 @@ export default class Pawn extends Piece {
                 moves.push(Square.at(location.row + 1, location.col))
             }
 
-
         } else {
+            //last row
             if (location.row === 0) return moves
 
+            //capture white piece
+            let pieceToCaptureRight = board.getPiece(Square.at(location.row - 1, location.col + 1))
+            let pieceToCaptureLeft = board.getPiece(Square.at(location.row - 1, location.col - 1))
+
+            if (pieceToCaptureLeft && pieceToCaptureLeft.player === Player.WHITE || pieceToCaptureRight && pieceToCaptureRight.player === Player.WHITE) {
+                if (location.col !== 7) moves.push(Square.at(location.row - 1, location.col + 1))
+                if (location.col !== 0) moves.push(Square.at(location.row - 1, location.col - 1))
+            }
+
+            //limit movement if blocked
             let blocking1Square = board.getPiece(Square.at(location.row - 1, location.col))
             let blocking2Squares = board.getPiece(Square.at(location.row - 2, location.col))
 
